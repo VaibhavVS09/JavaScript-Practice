449acceb05f2ad33d02d92fbbcf8e9d3b6af8994
Vaibhav Satvekar <vaibhavsatvrkar09@gmail.com>
2024-07-03 19:22:14 +0530

    Added conversionOperations files

---
+++ 01_Basic/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score = "vaibhav";
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//notes
+//here the type of valueInNumber is number but we get this output like as below
+//"33" => 33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+//null=>0
+//undefined=>NaN
+//NaN=>NaN

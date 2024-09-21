69585f709448182e4e2f34dcc53743d74119e26e
Vaibhav Satvekar <vaibhavsatvrkar09@gmail.com>
2024-09-21 17:39:55 +0530

    Added some code
    Added some code

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,21 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+//function
+
+function fruitProcessor(apples, mango) {
+  const juice = `Juice with ${apples} apples and ${mango} mangoes.`;
+  return juice;
+}
+
+const applejuice = fruitProcessor(5, 0);
+console.log(applejuice);
+
+const mangoesjuice = fruitProcessor(0, 5);
+console.log(mangoesjuice);
